0f1a2aac581caded0a640b79bde67b89677c9d7c
21933 <angelo.llanos@vtsd.com>
2023-11-13 08:41:01 -0500

    1.2.1Example

--- 1.2.1/a121_catch_a_turtle_[AL].py
+++ 1.2.1/a121_catch_a_turtle_[AL].py
@@ -9,37 +9,9 @@ test1 = trtl.Turtle()
 #-----game configuration----
 spot = test1
 spot.speed(100)
+spot.shape("circle")
+spot.shapesize(5)
 spot.fillcolor("pink")
-def drawSquare():
-    spot.pendown()
-    spot.fd(50)
-    spot.right(90)
-    spot.fd(50)
-    spot.right(90)
-    spot.fd(50)
-    spot.right(90)
-    spot.fd(50)
-
-def drawTriangle(long, x, y):
-    test.penup()
-    test.goto(x, y)
-    test.pendown()
-    test.fd(long)
-    test.right(90)
-    test.fd(long)
-    test.right(90)
-    test.fd(long)
-    test.right(90)
-    test.fd(long)
-
-x = -100
-y = -100
-length = 43
-for tri in range(100):
-    drawTriangle(length, x, y )
-    x += length * 0.25
-    y += length * 0.25
-    length += 0.5
 
 #-----initialize turtle-----
 
