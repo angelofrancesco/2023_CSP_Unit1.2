1d81508e98f3c8fc5ec84da069d63c25773854ef
21933 <angelo.llanos@vtsd.com>
2023-11-21 10:07:07 -0500

    1.2.1Example

--- 1.2.1/a121_catch_a_turtle_[AL].py
+++ 1.2.1/a121_catch_a_turtle_[AL].py
@@ -1,22 +1,13 @@
 # a121_catch_a_turtle.py
 #-----import statements-----
 import turtle as trtl
-test = trtl.Turtle()
-spot = test
-
 
 
 #-----game configuration----
-spot.speed(100)
-spot.shape("circle")
-spot.shapesize(5)
-spot.fillcolor("pink")
+color = ("pink")
 
 #-----initialize turtle-----
-def spot_onclick():
 
-
-spot.onclick("circle")
 
 #-----game functions--------
 def spot_clicked(x, y):
